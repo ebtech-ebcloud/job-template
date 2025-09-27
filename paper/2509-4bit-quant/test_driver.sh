d42dad993fd7b2a990df780cb4fd8e5bb5ce7d6f
fengx <fengx@ebtech.com>
2025-09-27 08:15:19 +0800

    modify Dockerfile

--- paper/2509-4bit-quant/test_driver.sh
+++
@@ -1,7 +0,0 @@
-python -u -m torch.distributed.run --nproc_per_node=8 --rdzv_endpoint localhost:6000  --rdzv_backend c10d  all_reduce_bench.py
-for((i=0; i<8; i++)
-do
-export CUDA_VISIBLE_DEVICES=$i
-python3 mamf-finder.py  --m_range 0 20480 256 --n 4096 --k 4096 --output_file=$(date +"%Y-%m-%d-%H:%M:%S").txt
-done
-
