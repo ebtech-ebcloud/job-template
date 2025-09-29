7089e0bf2111ddd9bdc827d2b72586768765e4a8
FX <fengx@ebtech.com>
2025-09-29 17:26:14 +0800

    modify download_gsm.py

--- paper/2509-4bit-quant/download_gsm.py
+++ paper/2509-4bit-quant/download_gsm.py
@@ -1,5 +1,5 @@
 import os
-from datasets import load_dataset
 os.environ['HF_ENDPOINT']='https://hf-mirror.com'
+from datasets import load_dataset
 dataset = load_dataset("openai/gsm8k", "main")
-dataset.save_to_disk("gsm8k/")
+dataset.save_to_disk("/root/gsm8k/")
