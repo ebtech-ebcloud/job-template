492d3c3957e603374443fdd043c09d773a979257
FX <fengx@ebtech.com>
2025-10-16 16:28:04 +0800

    modify quant_fp4.py

--- paper/2509-4bit-quant/quant_fp4.py
+++ paper/2509-4bit-quant/quant_fp4.py
@@ -50,11 +50,7 @@ ds = ds.map(tokenize, remove_columns=ds.column_names)
 #   * quantize the weights to fp4 with per group 16 via ptq
 #   * calibrate a global_scale for activations, which will be used to
 #       quantize activations to fp4 on the fly
-#recipe = QuantizationModifier(targets="Linear", scheme="NVFP4", ignore=["lm_head"])
-recipe = [
-    AWQModifier(ignore=["lm_head"], scheme="W4A16_ASYM", targets=["Linear"]),
-]
-
+recipe = QuantizationModifier(targets="Linear", scheme="NVFP4", ignore=["lm_head"])
 # Save to disk in compressed-tensors format.
 SAVE_DIR = MODEL_ID.rstrip("/").split("/")[-1] + "-NVFP4"
 
