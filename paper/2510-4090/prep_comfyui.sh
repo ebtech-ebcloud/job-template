d5ec7b013f66c8a3cc4ddf81de8094c3213ea2f9
FX <fengx@ebtech.com>
2025-10-22 23:42:05 +0800

    add 2510-4090

---
+++ paper/2510-4090/prep_comfyui.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#准备ComfyUI环境，主要是需要 4 个模型文件
+#本应用需要两个clip文件
+cp /public/huggingface-models/openai/clip-vit-large-patch14/model.safetensors /data/ComfyUI/models/clip/clip-vit-large-patch14.safetensors
+cp /public/huggingface-models/comfyanonymous/flux_text_encoders/t5xxl_fp8_e4m3fn.safetensors /data/ComfyUI/models/clip/
+cp /public/huggingface-models/black-forest-labs/FLUX.1-dev/vae/diffusion_pytorch_model.safetensors  /data/ComfyUI/models/vae/
+export HF_ENDPOINT=https://hf-mirror.com
+huggingface-cli download city96/FLUX.1-dev-gguf --include flux1-dev-Q4_0.gguf --local-dir tmp
+mv tmp/flux1-dev-Q4_0.gguf /data/ComfyUI/models/unet/
+modelscope download --model FluxLora/XLabs-AI-flux-RealismLora --local_dir XLabs-AI-flux-RealismLora
+cp XLabs-AI-flux-RealismLora/lora.safetensors /data/ComfyUI/models/loras/flux_realism_lora.safetensors
