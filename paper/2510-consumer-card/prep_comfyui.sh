ef16ee019f1b538fa8300e54dff46240e0c0e633
root <root@gpu-a800-10.prod-huabei1.cluster.local>
2025-10-30 13:04:25 +0800

    modify prep_comfyui.sh

--- paper/2510-consumer-card/prep_comfyui.sh
+++ paper/2510-consumer-card/prep_comfyui.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 #假定数据盘的挂载点为/data/,把ComfyUI拷贝到/data,避免模型文件太大撑爆系统盘
-cp -r /root/ComfyUI /data/
 source /miniconda/bin/activate comfyui
+cp -r /root/ComfyUI /data/
 #准备ComfyUI需要的模型，主要是需要 5个模型文件:2个clip文件，1 个vae文件, 1个unet文件, 1个lora文件
 #其中有两个文件在拷贝后需要改名
-cp /public/huggingface-models/openai/clip-vit-large-patch14/model.safetensors /data/ComfyUI/models/clip/clip-vit-large-patch14.safetensors
-cp /public/huggingface-models/comfyanonymous/flux_text_encoders/t5xxl_fp8_e4m3fn.safetensors /data/ComfyUI/models/clip/
-cp /public/huggingface-models/black-forest-labs/FLUX.1-dev/vae/diffusion_pytorch_model.safetensors  /data/ComfyUI/models/vae/
-cp /public/huggingface-models/city96/FLUX.1-dev-gguf/flux1-dev-Q4_0.gguf /data/ComfyUI/models/unet/
-cp /public/huggingface-models/XLabs-AI/flux-RealismLora/lora.safetensors /data/ComfyUI/models/loras/flux_realism_lora.safetensors
+#cp /public/huggingface-models/openai/clip-vit-large-patch14/model.safetensors /public/shared-resources/cache/ComfyUI/models/clip/clip-vit-large-patch14.safetensors
+#cp /public/huggingface-models/comfyanonymous/flux_text_encoders/t5xxl_fp8_e4m3fn.safetensors /public/shared-resources/cache/ComfyUI/models/clip/
+#cp /public/huggingface-models/black-forest-labs/FLUX.1-dev/vae/diffusion_pytorch_model.safetensors  /public/shared-resources/cache/ComfyUI/models/vae/
+#cp /public/huggingface-models/city96/FLUX.1-dev-gguf/flux1-dev-Q4_0.gguf /public/shared-resources/cache/ComfyUI/models/unet/
+#cp /public/huggingface-models/XLabs-AI/flux-RealismLora/lora.safetensors /public/shared-resources/cache/ComfyUI/models/loras/flux_realism_lora.safetensors
+cp -r /public/shared-resources/cache/ComfyUI/* /root/ComfyUI/
 #如果需要自己下载模型，可以采用如下方式
 #modelscope download --model FluxLora/XLabs-AI-flux-RealismLora --local_dir XLabs-AI-flux-RealismLora
 #cp XLabs-AI-flux-RealismLora/lora.safetensors /data/ComfyUI/models/loras/flux_realism_lora.safetensors
--- paper/2510-consumer-card/start_owu.sh
+++ paper/2510-consumer-card/start_owu.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 source /miniconda/bin/activate sglang
 nohup python3 -m sglang.launch_server --model  /public/huggingface-models/\
-Qwen/Qwen3-8B-FP8/  --tp 1 --trust-remote-code --mem-fraction-static 0.65 >log_sglang 2>&1 &
+Qwen/Qwen3-8B-FP8/  --tp 1 --trust-remote-code --mem-fraction-static 0.65 >log_sglang.txt 2>&1 &
 source /miniconda/bin/activate open-webui
 export MINERU_MODEL_SOURCE=modelscope
 export MINERU_VIRTUAL_VRAM_SIZE=6
-nohup mineru-api --host 0.0.0.0 --port 8000 >log_mineru 2>&1 &
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+nohup mineru-api --host 0.0.0.0 --port 8000 >log_mineru.txt 2>&1 &
 cd /root/open-webui/backend
 export HF_ENDPOINT=https://hf-mirror.com
-nohup sh dev.sh >log_owu 2>&1 &
+nohup sh dev.sh >/root/log_owu.txt 2>&1 &
